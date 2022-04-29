271f13009eec5319efa3babdcbcf471d1902029a
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-04-29 14:57:57 -0700

    user model

--- models/User.js
+++ models/User.js
@@ -4,21 +4,48 @@ const UserSchema = new Schema({
   username: {
     type: String,
     unique: true,
-    required: 'Username is required',
+    required: true,
     trim:true
   },
 
   email: {
     type: String,
     unique: true,
-    required: 'Email is required',
-    match: [/.+@.+\..+/]
+    required: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    "Please fill a valid email address"]
   },
 
+  thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+      }
+  ],
+
+  friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+  ]
+},
+
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+     // prevents virtuals from creating duplicate of _id as `id`
+    id: false
 });
+
+//get total count of friends on retrieval
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
 
 //create the User model using UserSchema
 const User = model('User', UserSchema);
-
 // then export it
 module.exports = User;
